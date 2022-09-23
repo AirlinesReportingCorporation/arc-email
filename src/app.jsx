d52773cfa5b5c9dd92f68a264838e51d1f64a41c
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-09-23 14:15:57 -0400

    jumbo update

--- src/app.jsx
+++ src/app.jsx
@@ -10,10 +10,18 @@ class App extends Component {
       <div className="arc-advanced-analytics-page">
         <div className="arc-advanced-analytics-jumbo-container">
           <div className="arc-advanced-analytics-jumbo">
-            <div className="row align-items-center">
-              <div className="col-lg-7">
+            <div className="row align-items-center m-auto">
+              <div className="col-lg-12">
                 <div className="arc-advanced-analytics-title">
-                  <h1 className="arc-advanced-h1">Advanced data analytics using Artificial Intelligence</h1>
+                  <h1 className="arc-advanced-h1">Advanced data analytics <br /> using Artificial Intelligence</h1>
+                  <div className="ctaBtn arc-advanced-btn">Test</div>
+                </div>
+              </div>
+            </div>
+            <div className="row align-items-center m-auto">
+              <div className="col-lg-12">
+                <div className="arc-advanced-analytics-subtitle">
+                Solving the most complex business challenges <br></br> using <span>ARC's machine learning techniques and technology <br></br>provides innovative analytical travel-related data.</span>
                 </div>
               </div>
             </div>
