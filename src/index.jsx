ed6d78711b11fd2e116f1d4b2e502fb08988f822
Ian Fajardo <ianfajardozx@gmail.com>
2022-12-05 16:31:23 -0500

    update with dnd drop

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.jsx";
 import "./scss/main.scss";
 
-var mountNode = document.getElementById('app');
+var appNode = document.getElementById("app");
 
-ReactDOM.render(<App />, mountNode); 
+ReactDOM.render(<App />, appNode);
