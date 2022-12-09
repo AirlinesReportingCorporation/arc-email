b66b120eabe0f48335160af66eadcbbb45112598
Ian Fajardo <ianfajardozx@gmail.com>
2022-12-09 14:24:20 -0500

    add functionality to add to list

--- src/app.jsx
+++ src/app.jsx
@@ -56,7 +56,7 @@ function App() {
       component: <h1>Title</h1>,
     },
     {
-      id: "2",
+      id: "5",
       name: "Button",
       component: (
         <table
@@ -93,7 +93,7 @@ function App() {
       ),
     },
     {
-      id: "3",
+      id: "6",
       name: "tac-header",
       component: (
         <p>
@@ -106,17 +106,28 @@ function App() {
     },
   ]);
 
+  const [itemAddID, setItemAddID] = useState(1000);
+
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const [activeView, setActiveView] = useState("design");
-  const [activeHover, setActiveHover] = useState(999);
+  const [activeHover, setActiveHover] = useState("");
+  const [activeAdd, setActiveAdd] = useState("");
+  const [activePosition, setActivePositon] = useState("");
   const [hoverRef, isHovered] = useHover();
+
+  const setActiveAddIndex = (index, position) => {
+    console.log("active add", index);
+    setActiveAdd(index);
+    setActivePositon(position);
+    handleShow();
+  };
 
   const [markup, setMarkup] = useState("");
 
-  const email = items.map((item) => <div key={item.id}>{item.id}</div>);
+  const email = items.map((item, i) => <div key={item.id}>{item.id}</div>);
 
   //useEffect is for functions that need to run on every render
   useEffect(() => {
@@ -124,13 +135,15 @@ function App() {
       <div key={item.id}>{item.component}</div>
     ));
 
-    console.log(sensors);
+    //console.log(sensors);
 
     setMarkup(
       '<!doctype html> <html> <head> <meta name="viewport" content="width=device-width, initial-scale=1.0"/> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> <title>Simple Transactional Email</title> <style> /* ------------------------------------- GLOBAL RESETS ------------------------------------- */ /*All the styling goes here*/ img { border: none; -ms-interpolation-mode: bicubic; max-width: 100%; } body { background-color: #f6f6f6; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; } table { border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; } table td { font-family: sans-serif; font-size: 14px; vertical-align: top; } /* ------------------------------------- BODY & CONTAINER ------------------------------------- */ .body { background-color: #f6f6f6; width: 100%; } /* Set a max-width, and make it display as block so it will automatically stretch to that width, but will also shrink down on a phone or something */ .container { display: block; margin: 0 auto !important; /* makes it centered */ max-width: 580px; padding: 10px; width: 580px; } /* This should also be a block element, so that it will fill 100% of the .container */ .content { box-sizing: border-box; display: block; margin: 0 auto; max-width: 580px; padding: 10px; } /* ------------------------------------- HEADER, FOOTER, MAIN ------------------------------------- */ .main { background: #ffffff; border-radius: 3px; width: 100%; } .wrapper { box-sizing: border-box; padding: 20px; } .content-block { padding-bottom: 10px; padding-top: 10px; } .footer { clear: both; margin-top: 10px; text-align: center; width: 100%; } .footer td, .footer p, .footer span, .footer a { color: #999999; font-size: 12px; text-align: center; } /* ------------------------------------- TYPOGRAPHY ------------------------------------- */ h1, h2, h3, h4 { color: #000000; font-family: sans-serif; font-weight: 400; line-height: 1.4; margin: 0; margin-bottom: 30px; } h1 { font-size: 35px; font-weight: 300; text-align: center; text-transform: capitalize; } p, ul, ol { font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px; } p li, ul li, ol li { list-style-position: inside; margin-left: 5px; } a { color: #189bb0; text-decoration: underline; } /* ------------------------------------- BUTTONS ------------------------------------- */ .btn { box-sizing: border-box; width: 100%; } .btn > tbody > tr > td { padding-bottom: 15px; } .btn table { width: auto; } .btn table td { background-color: #ffffff; border-radius: 5px; text-align: center; } .btn a { background-color: #ffffff; border: solid 1px #189bb0; border-radius: 5px; box-sizing: border-box; color: #189bb0; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-decoration: none; text-transform: capitalize; } .btn-primary table td { background-color: #189bb0; } .btn-primary a { background-color: #189bb0; border-color: #189bb0; color: #ffffff; } /* ------------------------------------- OTHER STYLES THAT MIGHT BE USEFUL ------------------------------------- */ .last { margin-bottom: 0; } .first { margin-top: 0; } .align-center { text-align: center; } .align-right { text-align: right; } .align-left { text-align: left; } .clear { clear: both; } .mt0 { margin-top: 0; } .mb0 { margin-bottom: 0; } .preheader { color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0; } .powered-by a { text-decoration: none; } hr { border: 0; border-bottom: 1px solid #f6f6f6; margin: 20px 0; } /* ------------------------------------- RESPONSIVE AND MOBILE FRIENDLY STYLES ------------------------------------- */ @media only screen and (max-width: 620px) { table.body h1 { font-size: 28px !important; margin-bottom: 10px !important; } table.body p, table.body ul, table.body ol, table.body td, table.body span, table.body a { font-size: 16px !important; } table.body .wrapper, table.body .article { padding: 10px !important; } table.body .content { padding: 0 !important; } table.body .container { padding: 0 !important; width: 100% !important; } table.body .main { border-left-width: 0 !important; border-radius: 0 !important; border-right-width: 0 !important; } table.body .btn table { width: 100% !important; } table.body .btn a { width: 100% !important; } table.body .img-responsive { height: auto !important; max-width: 100% !important; width: auto !important; } } /* ------------------------------------- PRESERVE THESE STYLES IN THE HEAD ------------------------------------- */ @media all { .ExternalClass { width: 100%; } .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div { line-height: 100%; } .apple-link a { color: inherit !important; font-family: inherit !important; font-size: inherit !important; font-weight: inherit !important; line-height: inherit !important; text-decoration: none !important; } #MessageViewBody a { color: inherit; text-decoration: none; font-size: inherit; font-family: inherit; font-weight: inherit; line-height: inherit; } .btn-primary table td:hover { background-color: #34495e !important; } .btn-primary a:hover { background-color: #34495e !important; border-color: #34495e !important; } } </style> </head> <body> <span class="preheader">This is preheader text. Some clients will show this text as a preview.</span> <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body"> <tr> <td> </td> <td class="container"> <div class="content">  <table role="presentation" class="main">  <tr> <td class="wrapper"> <table role="presentation" border="0" cellpadding="0" cellspacing="0">' +
         ReactDOMServer.renderToStaticMarkup(email) +
         '</table> </td> </tr>  </table>   <div class="footer"> footer </div>  </div> </td> <td> </td> </tr> </table> </body> </html>'
     );
+
+    console.log(items);
   });
 
   const itemIds = useMemo(() => items.map((item) => item.id, [items]));
@@ -142,12 +155,47 @@ function App() {
     })
   );
 
-  //TODO addItem(index,position) create a function that will add a component object to the items list either before or after the index depending on the bottom that was clicked
+  //TODO addItem(index) create a function that will add a component object to the items list either before or after the index depending on the bottom that was clicked
   // index is the current index of the item you want to add to
-  // position is whehter it will be above or below the current index
-  const addItem = (index, position) => {
-    console.log("add");
-    console.log(index + ":" + position);
+  const addItem = (blockName) => {
+    console.log(
+      "Add " +
+        blockName +
+        " at the " +
+        activePosition +
+        " of index " +
+        activeAdd
+    );
+
+    var newId = itemAddID + 1;
+
+    var tempItems = [...items];
+
+    if (activePosition == "top") {
+      if (activeAdd == 0) {
+        tempItems = prepend(
+          {
+            id: newId,
+            name: blockName,
+            component: "asdf",
+          },
+          tempItems
+        );
+      } else {
+        //splice
+      }
+    } else if (activePosition == "bottom") {
+      if (activeAdd == items.length - 1) {
+        //append
+      } else {
+        //splice
+      }
+    }
+
+    setItems(tempItems);
+    setItemAddID(newId);
+    
+    handleClose();
   };
 
   //TODO modifyItem be able to modify a component after adding it to the list, it should be able to figure out what component it is and pull the corresponding custom form to edit
@@ -161,84 +209,134 @@ function App() {
         <div className="row no-gutters" style={{ maxWidth: "100%" }}>
           <div className="col-lg-1 arc-email-tools">
             <div className="arc-email-tool-container text-center">
-              <div className="arc-email-tool-icon active" style={{ fontSize: "2rem" }}>
+              <div
+                className={
+                  activeView == "design"
+                    ? "arc-email-tool-icon active"
+                    : "arc-email-tool-icon"
+                }
+                style={{ fontSize: "2rem" }}
+                onClick={() => setActiveView("design")}
+              >
                 <i class="fas fa-stream"></i>
               </div>
-              <div className="arc-email-tool-icon " style={{ fontSize: "2rem" }}>
+              <div
+                className={
+                  activeView == "download"
+                    ? "arc-email-tool-icon active"
+                    : "arc-email-tool-icon"
+                }
+                style={{ fontSize: "2rem" }}
+                onClick={() => setActiveView("download")}
+              >
                 <i class="fas fa-download"></i>
               </div>
-              <div className="arc-email-tool-icon " style={{ fontSize: "2rem" }}>
+              <div
+                className={
+                  activeView == "settings"
+                    ? "arc-email-tool-icon active"
+                    : "arc-email-tool-icon"
+                }
+                style={{ fontSize: "2rem" }}
+                onClick={() => setActiveView("settings")}
+              >
                 <i class="fas fa-cog"></i>
               </div>
             </div>
           </div>
           <div className="col-lg-3 arc-email-tool-items">
             <div className="arc-email-sidebar-container">
-              <DndContext
-                sensors={sensors}
-                collisionDetection={closestCenter}
-                onDragEnd={handleDragEnd}
+              <div
+                className="arc-email-sidebar-email-parts"
+                style={{ display: activeView === "design" ? "block" : "none" }}
               >
-                <SortableContext
-                  items={itemIds}
-                  strategy={verticalListSortingStrategy}
+                <DndContext
+                  sensors={sensors}
+                  collisionDetection={closestCenter}
+                  onDragEnd={handleDragEnd}
                 >
-                  {items.map((item, i) => (
-                    <div
-                      className="arc-email-sortable-item-container"
-                      onMouseOver={() => setActiveHover(item.id)}
-                      onMouseOut={() => setActiveHover("")}
-                    >
+                  <SortableContext
+                    items={itemIds}
+                    strategy={verticalListSortingStrategy}
+                  >
+                    {items.map((item, i) => (
                       <div
-                        className="arc-email-template-add add-top"
-                        onClick={handleShow}
-                        style={{
-                          display: activeHover == item.id ? "block" : "none",
-                        }}
+                        className="arc-email-sortable-item-container"
+                        onMouseOver={() => setActiveHover(item.id)}
+                        onMouseOut={() => setActiveHover("")}
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                          className="w-6 h-6"
+                        <div
+                          className="arc-email-template-add add-top"
+                          onClick={() => setActiveAddIndex(i, "top")}
+                          style={{
+                            display: activeHover == item.id ? "block" : "none",
+                          }}
                         >
-                          <path
-                            fillRule="evenodd"
-                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </div>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </div>
 
-                      <SortableItem
-                        key={item.id}
-                        id={item.id}
-                        name={item.name}
-                      ></SortableItem>
-                      <div
-                        className="arc-email-template-add add-bottom"
-                        style={{
-                          display: activeHover == item.id ? "block" : "none",
-                        }}
-                        onClick={handleShow}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                          className="w-6 h-6"
+                        <SortableItem
+                          key={item.id}
+                          id={item.id}
+                          name={item.name}
+                        ></SortableItem>
+                        <div
+                          className="arc-email-template-add add-bottom"
+                          style={{
+                            display: activeHover == item.id ? "block" : "none",
+                          }}
+                          onClick={() => setActiveAddIndex(i, "bottom")}
                         >
-                          <path
-                            fillRule="evenodd"
-                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </div>
                       </div>
-                    </div>
-                  ))}
-                </SortableContext>
-              </DndContext>
+                    ))}
+                  </SortableContext>
+                </DndContext>
+              </div>
+              <div
+                className="arc-email-sidebar-download-container"
+                style={{
+                  display: activeView === "download" ? "block" : "none",
+                }}
+              >
+                <h2>Download Code</h2>
+                <p>
+                  Copy the code below to your clipboard and upload to the HTML
+                  section of the email tool.
+                </p>
+                <textarea name="" id="" rows="10" value={markup}></textarea>
+              </div>
+              <div
+                className="arc-email-sidebar-email-parts"
+                style={{
+                  display: activeView === "settings" ? "block" : "none",
+                }}
+              >
+                Settings View
+              </div>
             </div>
           </div>
           <div className="col-lg-8">
@@ -246,9 +344,6 @@ function App() {
               style={{ width: "100%", minHeight: "100vh", border: "none" }}
               srcDoc={markup}
             ></iframe>
-          </div>
-          <div className="col-lg-12">
-            <textarea name="" id="" rows="10" value={markup}></textarea>
           </div>
         </div>
       </div>
@@ -261,7 +356,10 @@ function App() {
           <div className="row">
             {blockPreviews.map((block) => (
               <div className="col-lg-auto">
-                <div className="arc-email-component-add-container">
+                <div
+                  className="arc-email-component-add-container"
+                  onClick={() => addItem(block.name)}
+                >
                   <div className="arc-email-component-icon">{block.icon}</div>
                   <div className="arc-email-component-name">{block.name}</div>
                 </div>
@@ -329,6 +427,12 @@ function useHover() {
   );
 
   return [callbackRef, value];
+}
+
+function prepend(value, array) {
+  var newArray = array.slice();
+  newArray.unshift(value);
+  return newArray;
 }
 
 export default App;
