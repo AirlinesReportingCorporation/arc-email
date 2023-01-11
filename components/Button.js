5b2d5c028a9c83abeecdc9ff1910d7a4b6d01b44
Bree Armstrong <brianna.m.armstrong@gmail.com>
2023-01-11 16:19:06 -0500

    adjust style and formatting to fit as a tr instead of a table

--- components/Button.js
+++ components/Button.js
@@ -2,36 +2,45 @@ import React from "react";
 
 export default function Button(props) {
   return (
-    <table
-      role="presentation"
-      border="0"
-      cellpadding="0"
-      cellspacing="0"
-      className="btn btn-primary"
-    >
-      <tbody>
-        <tr>
-          <td align="left">
-            <table
-              role="presentation"
-              border="0"
-              cellpadding="0"
-              cellspacing="0"
-            >
-              <tbody>
-                <tr>
-                  <td>
-                    {" "}
-                    <a href={props.link} target="_blank">
-                      {props.text}
-                    </a>{" "}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+      <tr>
+        <td style={{ paddingLeft: "25px", paddingRight: "25px", paddingTop:"5px", paddingBottom: "5px"}}>
+          <table
+            role="presentation"
+            border="0"
+            cellpadding="0"
+            cellspacing="0"
+            className="btn btn-primary"
+            width="100%"
+          >
+            <tbody>
+              <tr height="40px"></tr>
+              <tr>
+                <td>
+                  <table
+                    role="presentation"
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    align="center"
+                    style={{backgroundColor: "#189bb0", border: "2px solid #189bb0",width:"135px", height: "50px"}}
+                  >
+                    <tbody>
+                      <tr>
+                        <td align="center" style={{fontFamily: "Arial, sans-serif", fontSize:"13px", color: "#fff"}}>
+                          {" "}
+                          <a href={props.link} style={{textDecoration: "none", color: "#fff", display:"block", lineHeight: "48px",fontWeight: "bold"}} target="_blank">
+                            {props.text}
+                          </a>{" "}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+              <tr height="40px"></tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
   );
 }
