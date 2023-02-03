14b8887afbc9499de437c13feb555ec17a22bbc4
Ian Fajardo <ianfajardozx@gmail.com>
2023-02-03 10:51:46 -0500

    update with new spacer

--- components/Spacer.js
+++ components/Spacer.js
@@ -3,7 +3,21 @@ import React from "react";
 export default function Spacer(props) {
   return (
     <tr>
-      <td class="em_height" style={{lineHeight: "0", fontSize: "0"}} height={props.height ? props.height : "60"}>
+      <td
+        class="em_height"
+        align="left"
+        valign="top"
+        width="100%"
+        height={props.height ? props.height.replace("px", "") : "60"}
+        colspan="3"
+        style={{
+          "border-collapse": "collapse",
+          "mso-table-lspace": "0pt",
+          "mso-table-rspace": "0pt",
+          "mso-line-height-rule": "exactly",
+          "line-height": props.height ? props.height.replace("px", "") : "60" + "px",
+        }}
+      >
         &nbsp;
       </td>
     </tr>
