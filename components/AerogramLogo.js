edbde362cdf7c811ec472275fb28e29cce2261c6
Bree Armstrong <brianna.m.armstrong@gmail.com>
2023-03-14 15:54:23 -0400

    follow structure of old tac email maker

--- components/AerogramLogo.js
+++ components/AerogramLogo.js
@@ -3,67 +3,89 @@ import React from "react";
 export default function AerogramLogo(props) {
   return (
     <tr>
-      <td>
-        <tr>
-          <td class="em_height" height="20">
-            &nbsp;
-          </td>
-        </tr>
-        <tr>
-          <td height="75" valign="middle">
-            <a
-              href={props.link ? props.link : "https://www2.arccorp.com"}
-              target="_blank"
-              style={{
-                display: "block",
-              }}
-            >
-              <img
-                height="75"
-                style={{
-                  height: "75px",
-                  display: "block",
-                  marginRight: "auto",
-                  paddingLeft: "25px",
-                }}
-                src={
-                  "https://www2.arccorp.com/globalassets/email/aerogram-logo.jpg"
-                }
-              />
-            </a>
-          </td>
-          <td height="75" valign="middle">
-            <a
-              href={props.link ? props.link : "https://www2.arccorp.com"}
-              target="_blank"
-              style={{
-                display: "block",
-              }}
-            >
-              <img
-                height="23"
-                width="58"
-                style={{
-                  height: "23px",
-                  width: "58px",
-                  display: "block",
-                  marginLeft: "auto",
-                  paddingRight: "25px",
-                }}
-                src={
-                  "https://www2.arccorp.com/globalassets/arc-logos/corporate-logos/arc-logo-l-" +
-                  (props.color ? props.color : "teal") +
-                  ".png"
-                }
-              />
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td class="em_height" height="20">
-            &nbsp;
-          </td>
-        </tr>
+      <td width="478" align="center" valign="top">
+        <table
+          align="left"
+          border="0"
+          cellpadding="0"
+          cellspacing="0"
+          class="em_wrapper"
+          style={{ width: "96px" }}
+          width="96"
+        >
+          <tbody>
+            <tr>
+              <td align="center" class="em_pad_bottom" valign="top">
+                <a
+                  href="https://www2.arccorp.com/"
+                  name="www2_arccorp_com_"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                >
+                  <img
+                    alt="ARC"
+                    height="78"
+                    name="ARC-logo-header"
+                    src="https://www2.arccorp.com/globalassets/email/aerogram-logo.jpg"
+                    style={{
+                      display: "block",
+                      border: "none",
+                      fontFamily: "Arial, sans-serif",
+                      fontSize: "16px",
+                      lineHeight: "27px",
+                      color: "#000000",
+                      paddingLeft: "25px",
+                    }}
+                    title="ARC"
+                    width="148"
+                  ></img>
+                </a>
+              </td>
+              <td class="em_hide" width="25">
+                &nbsp;
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table
+          align="right"
+          border="0"
+          cellpadding="0"
+          cellspacing="0"
+          class="em_wrapper"
+        >
+          <tbody>
+            <tr>
+              <td align="center" class="em_pad_bottom" valign="top">
+                <a
+                  href="https://www2.arccorp.com/"
+                  name="www2_arccorp_com_"
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                >
+                  <img
+                    alt="ARC"
+                    height="78"
+                    name="ARC-logo-header"
+                    src="https://www2.arccorp.com/globalassets/email/aerogram-arc.jpg"
+                    style={{
+                      display: "block",
+                      border: "none",
+                      fontFamily: "Arial, sans-serif",
+                      fontSize: "16px",
+                      lineHeight: "27px",
+                      color: "#000000",
+                      paddingRight: "25px",
+                    }}
+                    title="ARC"
+                    width="40"
+                  ></img>
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </td>
     </tr>
   );
