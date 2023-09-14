77024087de7bac4d5eae9064f1735424ab729715
Ian Fajardo <ifajardo@arccorp.com>
2023-09-14 10:28:16 -0400

    update standard header with two column code adjust

--- components/StandardHeader.js
+++ components/StandardHeader.js
@@ -15,158 +15,158 @@ export default function StandardHeader(props) {
           valign="top"
         >
           <table
-            align="center"
+            align="left"
             border="0"
             cellPadding="0"
             cellSpacing="0"
-            width="100%"
+            className="em_wrapper"
+            style={{ width: "629px" }}
+            width="629"
           >
             <tbody>
               <tr>
-                <td className="em_height" height="20">
+                <td
+                  colSpan={2}
+                  class="em_height"
+                  align="left"
+                  valign="top"
+                  width="100%"
+                  height="20"
+                  style={{
+                    "border-collapse": "collapse",
+                    "mso-table-lspace": "0pt",
+                    "mso-table-rspace": "0pt",
+                    "mso-line-height-rule": "exactly",
+                    "line-height": "20px",
+                  }}
+                >
                   &nbsp;
                 </td>
               </tr>
               <tr>
-                <td align="center" valign="top">
+                <td width="314" align="center" valign="top">
                   <table
                     align="left"
                     border="0"
                     cellPadding="0"
                     cellSpacing="0"
                     className="em_wrapper"
-                    style={{ width: "629px" }}
-                    width="629"
+                    style={{ width: "96px" }}
+                    width="96"
                   >
                     <tbody>
                       <tr>
-                        <td width="314" align="center" valign="top">
-                          <table
-                            align="left"
-                            border="0"
-                            cellPadding="0"
-                            cellSpacing="0"
-                            className="em_wrapper"
-                            style={{ width: "96px" }}
-                            width="96"
+                        <td
+                          align="center"
+                          className="em_pad_bottom"
+                          valign="top"
+                        >
+                          <a
+                            href="https://www2.arccorp.com/"
+                            name="www2_arccorp_com_"
+                            style={{
+                              textDecoration: "none !important",
+                            }}
+                            target="_blank"
                           >
-                            <tbody>
-                              <tr>
-                                <td
-                                  align="center"
-                                  className="em_pad_bottom"
-                                  valign="top"
-                                >
-                                  <a
-                                    href="https://www2.arccorp.com/"
-                                    name="www2_arccorp_com_"
-                                    style={{
-                                      textDecoration: "none !important",
-                                    }}
-                                    target="_blank"
-                                  >
-                                    <img
-                                      alt="ARC"
-                                      height="20"
-                                      name="ARC-logo-header"
-                                      src={
-                                        "https://www2.arccorp.com/globalassets/arc-logos/corporate-logos/arc-logo-l-" +
-                                        (props.color == "white" ? "teal": "white") +
-                                        ".png"
-                                      }
-                                      style={{
-                                        display: "block",
-                                        border: "none",
-                                        fontFamily: "Arial, sans-serif",
-                                        fontSize: "16px",
-                                        lineHeight: "27px",
-                                        color: "#000000",
-                                      }}
-                                      title="ARC"
-                                      width="52"
-                                    />
-                                  </a>
-                                </td>
-                                <td className="em_hide" width="25">
-                                  &nbsp;
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
+                            <img
+                              alt="ARC"
+                              height="20"
+                              name="ARC-logo-header"
+                              src={
+                                "https://www2.arccorp.com/globalassets/arc-logos/corporate-logos/arc-logo-l-" +
+                                (props.color == "white" || props.color == ""
+                                  ? "teal"
+                                  : "white") +
+                                ".png"
+                              }
+                              style={{
+                                display: "block",
+                                border: "none",
+                                fontFamily: "Arial, sans-serif",
+                                fontSize: "16px",
+                                lineHeight: "27px",
+                                color: "#000000",
+                              }}
+                              title="ARC"
+                              width="52"
+                            />
+                          </a>
                         </td>
-                        <td width="314" align="right" valign="top">
-                          <table
-                            align="right"
-                            border="0"
-                            cellPadding="0"
-                            cellSpacing="0"
-                            className="em_wrapper"
-                            width="314"
+                        <td className="em_hide" width="25">
+                          &nbsp;
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td width="314" align="right" valign="top">
+                  <table
+                    align="right"
+                    border="0"
+                    cellPadding="0"
+                    cellSpacing="0"
+                    className="em_wrapper"
+                    width="314"
+                  >
+                    <tbody>
+                      <tr>
+                        <td
+                          className="title_center"
+                          align="right"
+                          height="20"
+                          style={{
+                            fontFamily: "Arial, sans-serif",
+                            fontSize: "12px",
+                            color:
+                              props.color == "white" ? "#189bb0" : "#FFFFFF",
+                            lineHeight: "14px",
+                            textAlign: "right",
+                          }}
+                          valign="middle"
+                        >
+                          <a
+                            name="THE INTELLIGENCE BEHIND AIR TRAVEL"
+                            style={{
+                              fontFamily: "Arial, sans-serif",
+                              fontSize: "12px",
+                              color:
+                                props.color === "white"
+                                  ? "#189bb0"
+                                  : "#FFFFFF !important",
+                              lineHeight: "14px",
+                              textDecoration: "none !important",
+                            }}
+                            href="https://www2.arccorp.com/articles-trends/the-latest/"
                           >
-                            <tbody>
-                              <tr>
-                                <td
-                                  className="title_center"
-                                  align="right"
-                                  height="20"
-                                  style={{
-                                    fontFamily: "Arial, sans-serif",
-                                    fontSize: "12px",
-                                    color:
-                                      props.color == "white"
-                                        ? "#189bb0"
-                                        : "#FFFFFF",
-                                    lineHeight: "14px",
-                                    textAlign: "right",
-                                  }}
-                                  valign="middle"
-                                >
-                                  <a
-                                    name="THE INTELLIGENCE BEHIND AIR TRAVEL"
-                                    style={{
-                                      fontFamily: "Arial, sans-serif",
-                                      fontSize: "12px",
-                                      color:
-                                        props.color === "white"
-                                          ? "#189bb0"
-                                          : "#FFFFFF !important",
-                                      lineHeight: "14px",
-                                      textDecoration: "none !important",
-                                    }}
-                                    href="https://www2.arccorp.com/articles-trends/the-latest/"
-                                  >
-                                    <span
-                                      style={{
-                                        fontFamily: "Arial, sans-serif",
-                                        fontSize: "12px",
-                                        color:
-                                          props.color === "white"
-                                            ? "#189bb0"
-                                            : "#FFFFFF !important",
-                                        lineHeight: "14px",
-                                        textDecoration: "none !important",
-                                      }}
-                                    >
-                                      <font
-                                        style={{
-                                          fontFamily: "Arial, sans-serif",
-                                          fontSize: "12px",
-                                          color:
-                                            props.color === "white"
-                                              ? "#189bb0"
-                                              : "#FFFFFF !important",
-                                          lineHeight: "14px",
-                                          textDecoration: "none !important",
-                                        }}
-                                      >
-                                        THE INTELLIGENCE BEHIND AIR TRAVEL
-                                      </font>
-                                    </span>
-                                  </a>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
+                            <span
+                              style={{
+                                fontFamily: "Arial, sans-serif",
+                                fontSize: "12px",
+                                color:
+                                  props.color === "white"
+                                    ? "#189bb0"
+                                    : "#FFFFFF !important",
+                                lineHeight: "14px",
+                                textDecoration: "none !important",
+                              }}
+                            >
+                              <font
+                                style={{
+                                  fontFamily: "Arial, sans-serif",
+                                  fontSize: "12px",
+                                  color:
+                                    props.color === "white"
+                                      ? "#189bb0"
+                                      : "#FFFFFF !important",
+                                  lineHeight: "14px",
+                                  textDecoration: "none !important",
+                                }}
+                              >
+                                THE INTELLIGENCE BEHIND AIR TRAVEL
+                              </font>
+                            </span>
+                          </a>
                         </td>
                       </tr>
                     </tbody>
@@ -174,7 +174,21 @@ export default function StandardHeader(props) {
                 </td>
               </tr>
               <tr>
-                <td className="em_height" height="20">
+                <td
+                  colSpan={2}
+                  class="em_height"
+                  align="left"
+                  valign="top"
+                  width="100%"
+                  height="20"
+                  style={{
+                    "border-collapse": "collapse",
+                    "mso-table-lspace": "0pt",
+                    "mso-table-rspace": "0pt",
+                    "mso-line-height-rule": "exactly",
+                    "line-height": "20px",
+                  }}
+                >
                   &nbsp;
                 </td>
               </tr>
